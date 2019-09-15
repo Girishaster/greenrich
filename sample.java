5f1f2c951f749e7fa1f13944033594e2c2445237
girisha <girishaster@gmail.com>
2019-09-15 13:36:25 +0530

    some 2 l;ines

--- sample.java
+++ sample.java
@@ -2,7 +2,6 @@ class sample{
     public static void main(String args[]){
      System.out.println(" POLY SPACE FOR AUTOMATION ");
      System.out.println(" Powered by Developer - Girisha ");
-     System.out.println(" Powered by Developer - Girisha1 ");
     }
 }
 
