ee3f527bf8e0956d383330513ce1fa8df4c8e57b
girisha <girishaster@gmail.com>
2019-09-15 13:48:29 +0530

    1 liner

--- sample.java
+++ sample.java
@@ -1,7 +1,6 @@
 class sample{
     public static void main(String args[]){
      System.out.println(" POLY SPACE FOR AUTOMATION ");
-     System.out.println(" Powered by Developer - Girisha ");
     }
 }
 
