783b86b0af372373e28b75c2fbfe377c57c0a273
girisha <girishaster@gmail.com>
2019-09-15 13:05:54 +0530

    girisha1 committed

--- sample.java
+++ sample.java
@@ -2,6 +2,7 @@ class sample{
     public static void main(String args[]){
      System.out.println(" POLY SPACE FOR AUTOMATION ");
      System.out.println(" Powered by Developer - Girisha ");
+     System.out.println(" Powered by Developer - Girisha1 ");
     }
 }
 
