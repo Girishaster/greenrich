15c2cf3004bdd695a1378f7d6d2803cf753aba5c
girisha <girishaster@gmail.com>
2019-09-15 12:23:53 +0530

    latest code

--- sample.java
+++ sample.java
@@ -1,6 +1,7 @@
 class sample{
     public static void main(String args[]){
      System.out.println(" POLY SPACE FOR AUTOMATION ");
+     System.out.println(" Powered by Developer - Girisha ");
     }
 }
 
